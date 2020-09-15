466d29f16bdff3e23c3b2f2c85c05582a0a97b10
takuhagi <h.takumi52@gmail.com>
2020-09-15 14:22:13 +0900

    自動更新機能の実装

--- app/assets/javascripts/autoReload.js
+++ app/assets/javascripts/autoReload.js
@@ -2,36 +2,36 @@ $(function(){
   function buildHTML(message){
     if ( message.image ) {
       let html =
-        `<div class="MessageBox" data-message-id=${message.id}>
-          <div class="MessageInfo">
-            <div class="MessageInfo__userName">
+        `<div class="message__box" data-message-id=${message.id}>
+          <div class="message_info">
+            <div class="message_info__user__name">
               ${message.user_name}
             </div>
-            <div class="MessageInfo__date">
+            <div class="message_info__date">
               ${message.created_at}
             </div>
           </div>
-          <div class="Message">
-            <p class="Message__content">
+          <div class="message">
+            <p class="message__content">
               ${message.content}
             </p>
-            <img class="Message__image" src="${message.image}">
+            <img class="message__image" src="${message.image}">
           </div>
         </div>`
       return html;
     } else {
       let html =
-      `<div class="MessageBox" data-message-id=${message.id}>
-        <div class="MessageInfo">
-          <div class="MessageInfo__userName">
+      `<div class="message__box" data-message-id=${message.id}>
+        <div class="message_info">
+          <div class="message_info__user__name">
             ${message.user_name}
           </div>
-          <div class="MessageInfo__date">
+          <div class="message_info__date">
             ${message.created_at}
           </div>
         </div>
-        <div class="Message">
-          <p class="Message__content">
+        <div class="message">
+          <p class="message__content">
             ${message.content}
           </p>
         </div>
@@ -42,28 +42,32 @@ $(function(){
 
   let reloadMessages = function() {
     //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
-    let last_message_id = $('.MessageBox:last').data("message-id") || 0;
+    let last_message_id = $('.message__box:last').data("message-id") || 0;
+    console.log(last_message_id);
     $.ajax({
       //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
       url: "api/messages",
       //ルーティングで設定した通りhttpメソッドをgetに指定
-      type: 'get',
+      type: 'GET',
       dataType: 'json',
       //dataオプションでリクエストに値を含める
       data: {id: last_message_id}
     })
     .done(function(messages) {
       // 更新するメッセージがなかった場合は.doneの後の処理が動かないようにする
+        // console.log(messages);
+        // console.log(messages.length);
       if (messages.length !== 0) {
+        console.log(messages);
         //追加するHTMLの入れ物を作る
         let insertHTML = '';
         //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
-        $.each(messages, function(i, message) {
+        $.each(messages, function(i,message) {
           insertHTML += buildHTML(message)
         });
         //メッセージが入ったHTMLに、入れ物ごと追加
-        $('.MessageField').append(insertHTML);
-        $('.MessageField').animate({ scrollTop: $('.MessageField')[0].scrollHeight});
+        $('.main_chat__message__list').append(insertHTML);
+        $('.main_chat__message__list').animate({ scrollTop: $('.main_chat__message__list')[0].scrollHeight});
       }
     })
     .fail(function() {
