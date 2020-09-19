0f9219498e8cf57f47c08e0253c42bb5d5b3a29d
takuhagi <h.takumi52@gmail.com>
2020-09-20 08:39:26 +0900

    ツイート機能の実装2

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
   resources :users, only: [:index, :edit, :update]
-  resources :tweets, only: [:index, :new, :create, :destroy, :edit, :update, :show]
+  resources :tweets
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
-    # pictweet
+    
     resources :tweets do
       resources :comments, only: :create
     end
