24646439d8b8389c34668fe8c07c5302b87e1d37
takuhagi <h.takumi52@gmail.com>
2020-10-01 12:44:34 +0900

    フロント修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,7 @@ class User < ApplicationRecord
   
   # pictweet
   has_many :tweets
-  has_many :comments  # commentsテーブルとのアソシエーション
-
+  
 
   def self.search(input, id)
     return nil if input == ""
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :edit, :update]
   resources :tweets
   resources :groups, only: [:new, :create, :edit, :update] do
-  resources :messages, only: [:index, :create]
+    resources :messages, only: [:index, :create]
     
 #追加
     namespace :api do
